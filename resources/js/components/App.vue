7bed4ec3c2bd18a3a0db2ff80ff28cf0d07a7f45
David Rodrigo Aymara Cabrera <231156@unamba.edu.pe>
2025-12-01 19:22:02 -0500

    commit 1

---
+++ resources/js/components/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1 style="color: green; text-align: center;">
+        Vue + Laravel funcionando 🎉
+    </h1>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
